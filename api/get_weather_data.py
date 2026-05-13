1db077042f6b5824735a53e1c6a81f967b9ecd26
Abhinabk <abhinabk123@gmail.com>
2026-05-13 06:33:57 +0000

    retry with exp backoff not working need global cooldown

--- api/get_weather_data.py
+++ api/get_weather_data.py
@@ -7,9 +7,13 @@ from datetime import datetime,date
 import time 
 import random
 # open-meteo api call
+session  = requests.Session()
+
 def fetch_weather_daily(
-    station_code: str, latitude: float, longitude: float, start_date: str, end_date: str
+    station_code: str, latitude: float, longitude: float, start_date: str, end_date: str,
+    max_retries:int = 5
 ) -> dict[str, Any]:
+    
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
         "latitude": latitude,
@@ -29,35 +33,54 @@ def fetch_weather_daily(
             "relative_humidity_2m_mean",
         ],
     }
-    response = requests.get(url=url, params=params, timeout=(5, 30))
-    response.raise_for_status()
-    print(f"[FETCH] {station_code}")
-    return {"station_code": station_code,"weather": response.json()}
+    for attempt in range(max_retries):
+        try:
+            response = session.get(url=url, params=params, timeout=(5, 30))
+            #rate limted
+            if response.status_code == 429:
+                raise requests.HTTPError("429 Rate Limited")
 
-def flatten_weather_data(weather_data: list[dict]) -> list[dict]:
+            response.raise_for_status()
+            print(f"[FETCH] {station_code}")
+            return {"station_code": station_code,"weather": response.json()}
+        
+        except(requests.exceptions.HTTPError,
+                requests.exceptions.Timeout,
+                requests.exceptions.ConnectionError,) as e:
+            
+            if attempt == max_retries-1:
+                raise e 
+            #exp backoff
+            wait = (2 ** attempt) + random.uniform(1, 3) #jitter for thurering herd
+            print(f"[RETRY]attempt {attempt+1}/{max_retries} in {wait:.1f}s")
+            time.sleep(wait)
+
+    raise RuntimeError(f"Failed fetching {station_code}")
+
+def flatten_weather_data(weather_data: dict) -> list[dict]:
+    #run agianst single station at a time
     rows = []
-    for station in weather_data:
-        station_code = station["station_code"]
-        daily = station.get("weather", {}).get("daily")
-        if not daily:
-            continue
-
-        for i in range(len(daily["time"])):
-            rows.append(
-                {   
-                    "station-code": station_code,
-                    "date": daily["time"][i],
-                    "temperature_2m_max": daily["temperature_2m_max"][i],
-                    "temperature_2m_min": daily["temperature_2m_min"][i],
-                    "temperature_2m_mean": daily["temperature_2m_mean"][i],
-                    "precipitation_sum": daily["precipitation_sum"][i],
-                    "rain_sum": daily["rain_sum"][i],
-                    "wind_speed_10m_max": daily["wind_speed_10m_max"][i],
-                    "wind_gusts_10m_max": daily["wind_gusts_10m_max"][i],
-                    "relative_humidity_2m_mean": daily["relative_humidity_2m_mean"][i],
-                    "weather_code": daily["weather_code"][i]
-                }
-            )
+    station_code = weather_data["station_code"]
+    daily = weather_data.get("weather", {}).get("daily")
+    if not daily:
+        return rows
+    
+    for i in range(len(daily["time"])):
+        rows.append(
+            {   
+                "station-code": station_code,
+                "date": daily["time"][i],
+                "temperature_2m_max": daily["temperature_2m_max"][i],
+                "temperature_2m_min": daily["temperature_2m_min"][i],
+                "temperature_2m_mean": daily["temperature_2m_mean"][i],
+                "precipitation_sum": daily["precipitation_sum"][i],
+                "rain_sum": daily["rain_sum"][i],
+                "wind_speed_10m_max": daily["wind_speed_10m_max"][i],
+                "wind_gusts_10m_max": daily["wind_gusts_10m_max"][i],
+                "relative_humidity_2m_mean": daily["relative_humidity_2m_mean"][i],
+                "weather_code": daily["weather_code"][i]
+            }
+        )
 
     return rows
 
@@ -79,42 +102,46 @@ def build_weather_dataset(
             return
         
     station_df = pd.read_csv(station_coordinates_path)
+    station_df = station_df.drop_duplicates(subset=['longitude','latitude'])
     min_max_date = get_min_max_date.get_global_date_range(raw_csv_path)
-    start_date,end_date = min_max_date
     total_stations = len(station_df['station_code'])
+
     if not min_max_date:
         print("[WARN] No min/max dates found")
         return
     
-    weather_data = []
+    start_date,end_date = min_max_date
 
     print("[LOG] Fetching weather data from API...")
 
-    for _,row in station_df.iterrows():
+    for row in station_df.itertuples(index=False):
 
-        station_code = row["station_code"]
-        latitude = row["latitude"]
-        longitude = row["longitude"]
+        station_code = str(row.station_code)
+        latitude = float(str(row.latitude))
+        longitude = float(str(row.longitude))
         
 
         try:
-            data = fetch_weather_daily(station_code, latitude, longitude, start_date, end_date)
-            weather_data.append(data)
+            data = fetch_weather_daily(station_code,latitude,longitude,start_date,end_date)
+            rows = flatten_weather_data(data)
+            if rows:
+                df = pd.DataFrame(rows)
+                df.to_csv(path_to_weather_file,mode="a",header=not path_to_weather_file.exists(),index=False)
+            
             count+=1
-            print(f"[LOG] Fetched weather for station: {station_code} {count}/{total_stations}")
-            delay = random.uniform(1, 2)
+            print(f"[LOG] Fetched weather for {station_code} remaining: {count}/{total_stations}")
+            delay = random.uniform(2, 4)
             print(f"[LOG] Sleeping for {delay:.2f}s")
             time.sleep(delay)
-        except Exception as e:
+
+        except requests.exceptions.RequestException as e:
             need_to_fetch_again.add(station_code)
             print(f"[WARN] [{station_code}] API failed: {e}")
 
-    rows = flatten_weather_data(weather_data)
-    if not rows:
-        print("[WARN] No weather data collected")
-        return
-    df = pd.DataFrame(rows)
-    df.to_csv(path_to_weather_file, index=False)
+    if need_to_fetch_again:
+        print("\n[FAILED STATIONS]")
+        print(need_to_fetch_again)
+
     print(f"[DONE] Saved {path_to_weather_file.name}")
 
 
